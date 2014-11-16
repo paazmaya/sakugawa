d6568a2e871c8d9ade70188f2535a0ef9fbacf1e
Juga Paazmaya <olavic@gmail.com>
2014-11-17 00:08:42 +0200

    Command line option tuning and copied CSS handling from my closed pull request

--- bin/sakugawa.js
+++ bin/sakugawa.js
@@ -11,7 +11,10 @@
 'use strict';
 
 var Bossy = require('bossy'),
-  sakugawa = require('../lib');
+  sakugawa = require('../lib'),
+  fs = require('fs'),
+  path = require('path'),
+  util = require('util');
 
 var options = {
   h: {
@@ -19,21 +22,68 @@ var options = {
     alias: 'help',
     type: 'boolean'
   },
+  V: {
+    description: 'Show version information',
+    alias: 'version',
+    type: 'boolean'
+  },
   n: {
-    description: 'Show your name',
-    alias: 'name'
+    description: 'Maximum number of CSS selectors per output file',
+    alias: 'max-selectors',
+    type: 'number',
+    default: 4090
+  },
+  s: {
+    description: 'Output CSS file suffix',
+    alias: 'suffix',
+    type: 'number',
+    default: '_'
+  },
+  m: {
+    description: 'Media query handling, separation to different file (separate) or ignorance (ignore). By default included',
+    alias: 'media-queries',
+    type: 'string',
+    valid: ['separate', 'ignore']
   }
 };
 
 var args = Bossy.parse(options);
 
-
 if (args instanceof Error) {
-  console.error(args.message);
+  util.error(args.message);
   return;
 }
 
-if (args.h || !args.n) {
-  console.log(Bossy.usage(options, 'hello -n <name>'));
+if (args.h) {
+  util.puts(Bossy.usage(options, 'sakugawa [options] huge-stylesheet.css [more CSS files]'));
   return;
+}
+
+if (args.V) {
+  var json = fs.readFileSync(path.join(__dirname, '../package.json'), 'utf8');
+  var pkg = JSON.parse(json);
+  util.puts('v' + pkg.version);
+  return;
+}
+
+// files, an array or string
+if (args._) {
+  var files = args._;
+  if (typeof files === 'string') {
+    files = [files];
+  }
+  files = files.filter(function (file) {
+    return fs.existsSync(file);
+  });
+  util.log(util.inspect(files));
+  
+  var opts = {
+    maxSelectors: args.n,
+    mediaQueries: args.m
+  };
+  
+  sakugawa(files, opts);
+}
+else {
+  util.error('No files defined');
 }
