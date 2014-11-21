152db5193d4bee38d611d8ecc65cb94e6f4f28e0
Juga Paazmaya <paazmaya@yahoo.com>
2014-11-21 14:19:07 +0200

    Nodetime removal

--- bin/sakugawa.js
+++ bin/sakugawa.js
@@ -10,15 +10,6 @@
 
 'use strict';
 
-var nodetime = require('nodetime');
-
-nodetime.profile({
-  accountKey: '423d4d28f98e9c98dceb1781a6e19074a95a287a',
-  appName: 'Sakugawa',
-  proxy: 'http://nokes.nokia.com:8080'
-});
-
-
 // Usually first variables defined are the ones requiring native Node.js modules
 var fs = require('fs'),
   path = require('path'),
@@ -111,6 +102,3 @@ if (args._) {
 else {
   util.error('No files defined');
 }
-
-
-nodetime.destroy();
