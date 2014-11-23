91e509f5c231a0254f4229a785e86e177f9de857
Juga Paazmaya <paazmaya@yahoo.com>
2014-11-23 21:08:50 +0200

    Default options failed if not options used

--- tests/index_tape.js
+++ tests/index_tape.js
@@ -3,9 +3,12 @@ var test = require('tape');
 var sakugawa = require('../lib/index');
 
 test('timing test', function (t) {
-    t.plan(1);
+    t.plan(2);
 
     t.equal(typeof sakugawa, 'function');
+
+    var styles = 'body { color: rebeccapurple; }';
+    t.equal(sakugawa(styles), [styles]);
 
 });
 
